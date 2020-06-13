71bce2df5c96c04499343d4467b0f83b63dbf14e
Géssica Goulart <gessica_mariana12@hotmail.com>
2020-06-13 15:06:17 +0100

    Golden Type

--- src/org/academiadecodigo/floppybirds/Apple.java
+++ src/org/academiadecodigo/floppybirds/Apple.java
@@ -1,6 +1,5 @@
 package org.academiadecodigo.floppybirds;
 
-import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Ellipse;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
@@ -10,30 +9,52 @@ public class Apple {
     private Picture apple;
     private int col;
     private int row;
+    private String appleType;
 
     public Apple(Grid grid) {
+
+
+/*
         col = (int) (Math.random() * grid.getCols()) * grid.getCellSize() + grid.PADDING;
         row = (int) (Math.random() * grid.getRows()) * grid.getCellSize() + grid.PADDING;
         ellipse = new Ellipse(col, row, grid.getCellSize(), grid.getCellSize());
         ellipse.fill();
         ellipse.setColor(Color.RED);
+*/
+
 
         col = (int) (Math.random() * grid.getCols()) * grid.getCellSize() + grid.PADDING;
         row = (int) (Math.random() * grid.getRows()) * grid.getCellSize() + grid.PADDING;
-        apple = new Picture(col, row, "resources/apple3.png");
+        apple = new Picture(col, row, random());
         apple.draw();
 
-        col = (int) (Math.random() * grid.getCols()) * grid.getCellSize() + grid.PADDING;
+       /* col = (int) (Math.random() * grid.getCols()) * grid.getCellSize() + grid.PADDING;
         row = (int) (Math.random() * grid.getRows()) * grid.getCellSize() + grid.PADDING;
         apple = new Picture(col, row, "resources/apple5.png");
         apple.draw();
+*/
 
+    }
 
+    private String random() {
+        if ((int) (Math.random() * 100) < 70) {
+            appleType = "Red";
+            return "resources/apple3.png";
+        }
+        appleType = "Golden";
+        return "resources/apple5.png";
     }
 
     public void delete() {
-        ellipse.delete();
+        apple.delete();
     }
 
+    public Picture getApple() {
+        return apple;
+    }
+
+    public String getAppleType() {
+        return appleType;
+    }
 
 }
